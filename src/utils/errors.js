31fd681bea2063028cba9c5d57767af18ea1a687
aakashpatle10 <aakashpatle1009@gmail.com>
2025-12-01 00:06:15 +0530

    Delete src directory

--- src/utils/errors.js
+++
@@ -1,9 +0,0 @@
-class AppError extends Error {
-  constructor(message, statusCode, error = null) {
-    super(message);
-    this.statusCode = statusCode;
-    this.error = error;
-  }
-}
-
-export { AppError };
